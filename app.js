bab594e81c6a5f8384b8f91a1896553d4fe5d6aa
KimDaEun1031 <kimde1031@gmail.com>
2022-07-04 03:35:57 +0900

    chore: Initial Setup

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const createError = require("http-errors");
+
+const app = express();
+
+const indexRouter = require("./routes/index");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+    credential: "true",
+  })
+);
+
+app.use("/", indexRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.json({
+    stack: err.stack,
+    message: err.message,
+  });
+});
+
+module.exports = app;
