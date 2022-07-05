64c636ad1a0089491119dc51fd0e498a0f6cd416
KimDaEun1031 <kimde1031@gmail.com>
2022-07-05 19:41:29 +0900

    refactor: Authorization middleware

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -4,10 +4,9 @@ const User = require("../models/User");
 const catchAsync = require("../utils/catchAsync");
 
 exports.isLoggedIn = catchAsync(async (req, res, next) => {
-  const verifier = req.headers.authorization.split(" ")[0];
   const token = req.headers.authorization.split(" ")[1];
 
-  if (verifier === "Bearer" && token) {
+  if (token) {
     const { id } = jwt.verify(token, process.env.SECRET_KEY);
     const user = await User.findById(id).lean();
 
@@ -19,8 +18,8 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
   res.json({
     result: "error",
     error: {
-      message: "유효하지 않은 접근입니다.",
-      status: 400,
+      message: "Unauthorized",
+      status: 401,
     },
   });
 });
